83001ed14f5aa42546d3b4267df520bc3db6346b
jag0573 <jamesgonzalez2@my.unt.edu>
2019-10-09 01:33:43 -0500

    Day 1 of my 30 days

---
+++ Day_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    // Declare second integer, double, and String variables.
+    int x;
+    double y;
+    string z;
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    cin >> x;
+    cin >> y;
+    cin.ignore();
+    getline(cin, z);
+    // Print the sum of both integer variables on a new line.
+    cout << x + i << endl;
+    // Print the sum of the double variables on a new line.
+    cout << fixed << setprecision(1) << y + d << endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s << z;
+
+    return 0;
+}
