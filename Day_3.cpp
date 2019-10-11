e7155e3b2602822c956dc8efebfff41a63ac2b9a
James Gonzalez <jamesg1996@gmail.com>
2019-10-11 13:47:16 -0500

    Day 3 of My 30 days

---
+++ Day_3.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if(N % 2 == 1)
+    {
+        cout << "Weird\n";
+    }
+    else
+    {
+        if((N >= 2) && (N <= 5))
+        {
+            cout << "Not Weird";
+        }
+        else if ((N >= 6) && (N <= 20)) 
+        {
+            cout << "Weird\n";
+        }
+        else if(N > 20)
+        {
+            cout << "Not Weird\n";
+        }
+    }
+    return 0;
+}
